7bbf7f87c6d389ff7546b5d3f24b73ff0c7fcb38
Roland Tecson <roland_tecson@hotmail.com>
2019-01-16 09:08:31 -0800

    Initial Commit

---
+++ InterfaceBuilder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  InterfaceBuilder
+//
+//  Created by Roland on 2019-01-16.
+//  Copyright © 2019 Game of Apps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
